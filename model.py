21c1e4f969f65b62847088756a99175f8af2c63e
gdgrant <gdgrant@uchicago.edu>
2018-07-30 16:50:22 -0500

    Upgraded scope variability to include/exclude RNNs

--- model.py
+++ model.py
@@ -40,7 +40,9 @@ class Model:
             self.scope_dict = {}
             with tf.variable_scope(scope):
                 for name in var_prefixes:
-                    self.scope_dict[name] = tf.get_variable(name, initializer=par['var_inits'][scope][name])
+                    if not ('W_rnn' in name and scope not in par['rnn_scopes']):
+                        self.scope_dict[name] = tf.get_variable(name, initializer=par['var_inits'][scope][name])
+
                     if scope in par['input_scopes'] and 'out' in name:
                         self.scope_dict[name+'_'] = tf.get_variable(name+'_', initializer=par['var_inits'][scope][name])
             self.var_dict[scope] = self.scope_dict
@@ -56,25 +58,42 @@ class Model:
 
     def recurrent_layer(self, var_scope, x, h):
 
-        W_in  = self.var_dict[var_scope]['W_in']
-        W_rnn = self.var_dict[var_scope]['W_rnn']
-        b_rnn = self.var_dict[var_scope]['b_rnn']
+        if var_scope in par['rnn_scopes']:
 
-        W_rnn_eff = par['EI_matrix'][var_scope] @ tf.nn.relu(W_rnn) if par['EI'] else W_rnn
-        h_post = h
+            W_in  = self.var_dict[var_scope]['W_in']
+            W_rnn = self.var_dict[var_scope]['W_rnn']
+            b_rnn = self.var_dict[var_scope]['b_rnn']
 
-        h = tf.nn.relu((1-par['alpha_neuron'])*h \
-         + par['alpha_neuron']*(x @ W_in + h_post @ W_rnn_eff + b_rnn) \
-         + tf.random_normal(h.shape, 0., par['noise_rnn'], dtype=tf.float32))
+            W_rnn_eff = par['EI_matrix'][var_scope] @ tf.nn.relu(W_rnn) if par['EI'] else W_rnn
+            h_post = h
 
-        return h
+            h = tf.nn.relu((1-par['alpha_neuron'])*h \
+             + par['alpha_neuron']*(x @ W_in + h_post @ W_rnn_eff + b_rnn) \
+             + tf.random_normal(h.shape, 0., par['noise_rnn'], dtype=tf.float32))
+
+            return h
+
+        else:
+
+            W_in  = self.var_dict[var_scope]['W_in']
+            b_rnn = self.var_dict[var_scope]['b_rnn']
+
+            h = tf.nn.relu(x @ W_in + b_rnn + tf.random_normal(h.shape, 0., par['noise_rnn'], dtype=tf.float32))
+
+            return h
 
 
     def run_model(self):
 
-        for t in range(par['num_time_steps']):
+        self.outputs_dict = {}
+        for input_scope in par['input_scopes']:
+            self.outputs_dict[input_scope+'_mu'] = []
+            self.outputs_dict[input_scope+'_si'] = []
+            self.outputs_dict[input_scope+'_lat'] = []
+            for output_scope in par['output_scopes']:
+                self.outputs_dict[input_scope+'_to_'+output_scope] = []
 
-            self.outputs_dict = {}
+        for t in range(par['num_time_steps']):
 
             for input_scope in par['input_scopes']:
 
@@ -97,9 +116,9 @@ class Model:
                 latent = mu + tf.exp(0.5*si)*tf.random_normal(si.shape)
 
                 self.state_dict[input_scope]['self'].append(h)
-                self.outputs_dict[input_scope+'_mu']  = mu
-                self.outputs_dict[input_scope+'_si'] = si
-                self.outputs_dict[input_scope+'_lat'] = latent
+                self.outputs_dict[input_scope+'_mu'].append(mu)
+                self.outputs_dict[input_scope+'_si'].append(si)
+                self.outputs_dict[input_scope+'_lat'].append(latent)
 
                 for output_scope in par['output_scopes']:
 
@@ -114,17 +133,72 @@ class Model:
                     out = tf.nn.relu(h @ W_out + b_out)
 
                     self.state_dict[input_scope][output_scope].append(h)
-                    self.outputs_dict[input_scope+'_to_'+output_scope] = out
+                    self.outputs_dict[input_scope+'_to_'+output_scope].append(out)
 
 
     def optimize(self):
-        print('Maximize')
+
+        opt = AdamOpt.AdamOpt(tf.trainable_variables(), par['learning_rate'])
+        eps = 1e-7
+
+        # Putting together variable groups
+        encoder  = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='encoder')
+        decoder  = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='decoder')
+        VAE_vars = encoder + decoder
+
+        generator     = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
+        discriminator = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='discriminator')
+        GAN_vars      = generator + discriminator
+
+        task_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='solution')
+
+        # Task loss and training
+        task_loss_list = [mask*tf.nn.softmax_cross_entropy_with_logits_v2(logits=out, labels=target+eps) \
+            for out, target, mask in zip(self.outputs_dict['encoder_to_solution'], self.target_data, self.time_mask)]
+        self.task_loss = tf.reduce_mean(tf.stack(task_loss_list))
+
+        y_prob = [tf.nn.softmax(out) for out in self.outputs_dict['generator_to_solution']]
+        self.entropy_loss = tf.reduce_mean(tf.stack([-m*tf.reduce_mean(-p_i * tf.log(p_i)) for p_i, m in zip(y_prob, self.time_mask)]))
+
+        y_prob = [tf.nn.softmax(out) for out in self.outputs_dict['encoder_to_solution']]
+        self.entropy_loss_enc = tf.reduce_mean(tf.stack([-m*tf.reduce_mean(-p_i * tf.log(p_i)) for p_i, m in zip(y_prob, self.time_mask)]))
+
+        self.train_task         = opt.compute_gradients(self.task_loss, var_list=task_vars)
+        self.train_task_entropy = opt.compute_gradients(self.entropy_loss, var_list=task_vars)
 
 
-        quit()
+        # Autoencoder loss and training
+        recon_loss_list = [tf.square(out-target) for out, target in \
+            zip(self.outputs_dict['encoder_to_decoder'], self.input_data)]
+        self.recon_loss = tf.reduce_mean(tf.stack(recon_loss_list))
+
+        si = self.outputs_dict['encoder_si']
+        mu = self.outputs_dict['encoder_mu']
+        latent_loss_list = [par['act_latent_cost'] * -0.5 * tf.reduce_sum(1+si_t-tf.square(mu_t)-tf.exp(si_t), axis=-1) \
+            for mu_t, si_t in zip(mu, si)]
+        self.act_latent_loss = tf.reduce_mean(tf.stack(latent_loss_list))
+
+        self.train_VAE = opt.compute_gradients(self.recon_loss + self.act_latent_loss, var_list=VAE_vars)
 
 
+        # Discriminator loss and training
+        self.discr_gen_loss = tf.reduce_mean(tf.square(tf.stack(self.outputs_dict['generator_to_discriminator'], axis=0) - par['discriminator_gen_target']))
+        self.discr_act_loss = tf.reduce_mean(tf.square(tf.stack(self.outputs_dict['encoder_to_discriminator'], axis=0) - par['discriminator_act_target']))
 
+        self.gener_gen_loss = tf.reduce_mean(tf.square(tf.stack(self.outputs_dict['generator_to_discriminator'], axis=0) - par['discriminator_act_target']))
+        self.gener_act_loss = tf.reduce_mean(tf.square(tf.stack(self.outputs_dict['encoder_to_discriminator'], axis=0) - par['discriminator_gen_target']))
+
+        si = self.outputs_dict['generator_si']
+        mu = self.outputs_dict['generator_mu']
+        latent_loss_list = [par['gen_latent_cost'] * -0.5 * tf.reduce_sum(1+si_t-tf.square(mu_t)-tf.exp(si_t), axis=-1) \
+            for mu_t, si_t in zip(mu, si)]
+        self.gen_latent_loss = tf.reduce_mean(tf.stack(latent_loss_list))
+
+        self.generator_loss = self.gener_gen_loss + self.gener_act_loss + self.gen_latent_loss
+        self.discriminator_loss = self.discr_gen_loss + self.discr_act_loss
+
+        self.train_generator     = opt.compute_gradients(self.generator_loss, var_list=generator)
+        self.train_discriminator = opt.compute_gradients(self.discriminator_loss, var_list=discriminator)
 
 
 def main(save_fn=None, gpu_id=None):
@@ -140,7 +214,7 @@ def main(save_fn=None, gpu_id=None):
     # Define all placeholders
     x = tf.placeholder(tf.float32, [par['num_time_steps'], par['batch_size'], par['n_input']], 'stim')
     y = tf.placeholder(tf.float32, [par['num_time_steps'], par['batch_size'], par['n_output']], 'out')
-    m = tf.placeholder(tf.float32, [par['num_time_steps'], par['batch_size']], 'mask')
+    m = tf.placeholder(tf.float32, [par['num_time_steps'], par['batch_size'], 1], 'mask')
 
     # Set up stimulus
     stim = stimulus.MultiStimulus()
@@ -157,17 +231,22 @@ def main(save_fn=None, gpu_id=None):
         sess.run(tf.global_variables_initializer())
         t_start = time.time()
 
-        for i in range(par['num_train_batches']):
+        # Training autoencoder
+        for i in range(par['num_autoencoder_batches']):
 
             # Generate a batch of stimulus data for training
             name, stim_in, y_hat, mk, _ = stim.generate_trial(0)
 
             # Put together the feed dictionary
-            feed_dict = {x:stim_in, y:y_hat, m:mk}
+            feed_dict = {x:stim_in, y:y_hat, m:mk[...,np.newaxis]}
 
             # Run the model
-            _, task_loss, output = sess.run([model.train_op, model.task_loss, model.output], feed_dict=feed_dict)
-            print(i, '|', task_loss)
+            _, recon_loss, latent_loss = sess.run([model.train_VAE, model.recon_loss, model.act_latent_loss], feed_dict=feed_dict)
+
+            if i%200 == 0:
+                print(i, '|', 'Recon:', recon_loss, 'Latent:', latent_loss)
+
+
 
 
 if __name__ == '__main__':
